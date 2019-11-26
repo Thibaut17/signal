13bcaacc008fbddca6feeb629f496c4430d5cd38
Thibaut17 <34840217+Thibaut17@users.noreply.github.com>
2019-11-26 18:19:11 +0100

    Add files via upload

---
+++ example.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 27 13:29:18 2019
+
+@author: thibaut
+"""
+from signal_traitement import traitement as trait, lecture_oscillo
+name="example.csv"
+
+u1,u2,t = lecture_oscillo(name)
+
+s=trait(t,u1,u2)
+s.trace(xlim=0.005)
+s.FFT(f1=s.frequence(1),f2=s.frequence(i=2))
+s.passe_bas1()
+s.trace(xlim=0.005)
+s.FFT(f1=s.frequence(1),f2=s.frequence(i=2))
+
+del u1,u2,t,name
